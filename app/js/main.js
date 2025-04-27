f4f895d621a581b1f9732648cef946f8e30e40e0
Novemberman90 <alexledovit@gmail.com>
2025-04-27 03:45:11 +0300

    add rooms-tab block and fix header

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,33 @@
-$(function (params) {
-  $('.menu__btn').on('click', function() {
-    $('.menu__block').toggleClass('menu__block--active');
-    $('.menu__btn').toggleClass('menu__btn--active');
-    $('body').toggleClass('lock');
-  });
+$(document).ready(function () {
   
-   $(document).on('click', function(e) {
-      // Проверяем, что клик не произошел по меню или кнопке
-      if (!$(e.target).closest('.menu__block, .menu__btn').length) {
-        $('.menu__block').removeClass('menu__block--active');
-        $('.menu__btn').removeClass('menu__btn--active');
-        $('html').removeClass('lock');
-      }
+    $('.rooms-tabs__button').on('click', function(e) {
+    e.preventDefault(); // отменяем переход по ссылке
+
+    var tabId = $(this).data('tab');
+
+    $('.rooms-tabs__button').removeClass('rooms-tabs__button--active');
+    $(this).addClass('rooms-tabs__button--active');
+
+    $('.rooms-tabs__panel').removeClass('rooms-tabs__panel--active');
+    $('#' + tabId).addClass('rooms-tabs__panel--active');
+  });
+
+
+  $(function (params) {
+    $('.menu__btn').on('click', function() {
+      $('.menu__block').toggleClass('menu__block--active');
+      $('.menu__btn').toggleClass('menu__btn--active');
+      $('body').toggleClass('lock');
     });
-})
+    
+     $(document).on('click', function(e) {
+        // Проверяем, что клик не произошел по меню или кнопке
+        if (!$(e.target).closest('.menu__block, .menu__btn').length) {
+          $('.menu__block').removeClass('menu__block--active');
+          $('.menu__btn').removeClass('menu__btn--active');
+          $('html').removeClass('lock');
+        }
+      });
+  })
+
+});
